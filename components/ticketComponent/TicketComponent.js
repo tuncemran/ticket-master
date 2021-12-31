d22b42915d2cb6b37991950dc705401f7e04eccc
Tunc Emran <tuncemran@hotmail.com>
2021-12-31 13:16:00 +0300

    Next.js dynamic routing is added.

--- components/ticketComponent/TicketComponent.js
+++ components/ticketComponent/TicketComponent.js
@@ -4,23 +4,26 @@ import { Card, Icon, Image } from 'semantic-ui-react'
 
 export default ({ownerName,ownerFacebookAccount, ticketTitle, ticketDescription, ticketPriceInDollars, ticketAddress }) => {
     return(
-        <Card style={{margin: 0, float: "left", width: "300px", marginRight: "5%"}}>
-            <Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' wrapped ui={false} />
-            <Card.Content>
-                <Card.Header>{ticketTitle}</Card.Header>
-                <Card.Meta>
-                    <span className='date'>{ownerName} - <a href={ownerFacebookAccount}>Check Me On Facebook</a></span>
-                </Card.Meta>
-                <Card.Description>
-                    {ticketDescription}
-                </Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-                <a>
-                    <Icon name='dollar' />
-                    {ticketPriceInDollars}
-                </a>
-            </Card.Content>
-        </Card>
+        <a href={process.env.NODE_ENV === 'production' ? `https://ticket-master.surge.sh/ticket/${ticketAddress}` :`http://localhost:3000/ticket/${ticketAddress}`}>
+            <Card style={{margin: 0, float: "left", width: "300px", marginRight: "5%"}}>
+                <Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' wrapped ui={false} />
+                <Card.Content>
+                    <Card.Header>{ticketTitle}</Card.Header>
+                    <Card.Meta>
+                        <span className='date'>{ownerName} - <a href={ownerFacebookAccount}>Check Me On Facebook</a></span>
+                    </Card.Meta>
+                    <Card.Description>
+                        {ticketDescription}
+                    </Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                    <a>
+                        <Icon name='dollar' />
+                        {ticketPriceInDollars}
+                    </a>
+                </Card.Content>
+            </Card>
+        </a>
+
     )
 }
